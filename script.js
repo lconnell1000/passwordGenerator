4fd0503b7e57f93655b56e783ecf0b3e2696946e
lconnell1000 <lconnell1000@gmail.com>
2021-12-22 14:57:09 +1030

    need to get common characters function working

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ var special = false;
 var lowerCase = false;
 var upperCase = false;
 //set constants for the different character types
-const useNum = '0123456789';
-const useLow = 'abcdefghijklmnopqrstuvwxyz';
-const useUp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const useSpec = '!@#$%^&*();:.,<>';
+const NUM_CHAR = '0123456789';
+const LOWERCASE_CHAR = 'abcdefghijklmnopqrstuvwxyz';
+const UPPERCASE_CHAR = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const SPECIAL_CHAR = '!@#$%^&*();:.,<>';
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -22,14 +22,7 @@ function writePassword() {
 
 }
 
-//function for confirmations from the user
-//function userConfirm (x,y) {
-//let x = confirm ('Click ok if you would like to use' + "x" + 'cancel if not')
-//if (x) {
-//allPossibleChars = allPossibleChars.concat(y)
-//}
-//return;
-//}
+
 // create our generatePassword function to prompt the user to input acceptance criteria for the password
 
 function generatePassword() {
@@ -49,23 +42,23 @@ function generatePassword() {
   //check for numbers
   let numbs = confirm('Click ok if you would like to use numbers, cancel if not')
   if (numbs) {
-    allPossibleChars = allPossibleChars.concat(useNum)
+    allPossibleChars = allPossibleChars.concat(NUM_CHAR)
   }
 
   //check for special characters
   let special = confirm('Click ok if you would like to use special characters, cancel if not')
   if (special) {
-    allPossibleChars = allPossibleChars.concat(useSpec)
+    allPossibleChars = allPossibleChars.concat(SPECIAL_CHAR)
   }
   //check for lower case letters
   let lowerCase = confirm('Click ok if you would like to use lower case letters, cancel if not')
   if (lowerCase) {
-    allPossibleChars = allPossibleChars.concat(useLow)
+    allPossibleChars = allPossibleChars.concat(LOWERCASE_CHAR)
   }
   //check for upper case letters
   let upperCase = confirm('Click ok if you would like to use upper case letters, cancel if not')
   if (upperCase) {
-    allPossibleChars = allPossibleChars.concat(useUp)
+    allPossibleChars = allPossibleChars.concat(UPPERCASE_CHAR)
   }
 
 
@@ -73,22 +66,24 @@ function generatePassword() {
   if (numbs === false && special === false && lowerCase === false && upperCase === false) {
     return alert('You must select atleast one character type to include in your password, try again')
   }
- 
+
   var notValid = true;
   var validPassword = "";
-  while (notValid){
-    let pass = generatePossible(passLength,allPossibleChars);
+  while (notValid) {
+    let pass = generatePossible(passLength, allPossibleChars);
     let valid = checkPasswordValid(pass);
-    if (valid){
+    console.log(pass + " password");
+    console.log(valid + " if valid");
+    if (valid) {
       notValid = false;
       validPassword = pass;
     }
   }
-return validPassword;
+  return validPassword;
 }
 // function to loop to generate random selection of character based on the password length chosen by the user.
 //This will kepe looping until mainPassword legnth = allPossibleChars length
-function generatePossible(userLength,concatString) {
+function generatePossible(userLength, concatString) {
   var mainPassword = "";
   for (var n = 0; n < userLength; n++) {
     mainPassword += concatString.charAt(Math.floor(Math.random() * concatString.length));
@@ -99,77 +94,38 @@ function generatePossible(userLength,concatString) {
 // function to check if our password is valid and contains all the character sets
 // that the user has elected to contain
 function checkPasswordValid(passToCheck) {
-if (numbs = true) {
-  let check1 = commonChar(numbs,passToCheck);
-  if (check1 === true) 
-    return true;
-    else {
+  console.log(passToCheck + ": check password valid");
+  if (numbs) {
+    let check1 = commonChar(NUM_CHAR, passToCheck);
+    if (!check1) {
       return false;
     }
-}
-if (special = true) {
-  let check2 = commonChar(special,passToCheck)
-  if (check2 === true) 
-    return true;
-    else {
+  }
+  if (special) {
+    let check2 = commonChar(SPECIAL_CHAR, passToCheck);
+    if (!check2) {
       return false;
     }
-}
-if (upperCase = true) {
-  let check3 = commonChar(upperCase,passToCheck)
-  if (check3 === true) 
-    return true;
-    else {
+  }
+  if (upperCase) {
+    let check3 = commonChar(UPPERCASE_CHAR, passToCheck);
+    if (!check3) {
       return false;
     }
-}
-if (lowerCase = true) {
-  let check4 = commonChar(lowerCase,passToCheck)
-  if (check4 === true) 
-    return true;
-    else {
+  }
+  if (lowerCase) {
+    let check4 = commonChar(LOWERCASE_CHAR, passToCheck);
+    if (!check4) {
       return false;
     }
-}
-}
 
+  }
+  return true;
+}
 
 //function to check if two strings have common characters
 function commonChar(left, right) {
-  var left_map = {};
-  var right_map = {};
-  var index = 0;
-
-  while (index < left.length || index < right.length) {
-
-    // Check left array
-    if (index < left.length) {
-      var c = left[index];
-
-      left_map[c] = true;
-
-      // Check if it exists in the other map
-      if (right_map[c]) {
-        return true;
-      }
-    }    
-
-    // Check right array
-    if (index < right.length) {
-      var c = right[index];
-
-      right_map[c] = true;
-
-      // Check if it exists in the other map
-      if (left_map[c]) {
-        return true;
-      }
-    }
-
-   index++;
-  }
-
-  return false;
+  
 }
 
 
